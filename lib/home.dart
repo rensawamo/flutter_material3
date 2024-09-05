f628031dad2b8818523c53009dad57124937b71d
rensawamo <ren0112ngzk@gmail.com>
2024-09-05 10:33:52 +0900

    refactor: ディレクトリ

---
+++ lib/home.dart
@@ -0,0 +1,103 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_theme/widget/setting_switch_tile.dart';
+
+class Home extends ConsumerWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Theme Tips",
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        // スクロール可能にするために SingleChildScrollView で包む
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Theme Tips"),
+
+                /// 以下は、app.dart で設定したテーマが適応される例
+                ///
+                /// error が適応される例
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: "Error 有り",
+                    hintText: "Input something",
+                    errorText: "Error Message",
+                  ),
+                ),
+                const SizedBox(height: 16),
+
+                /// surface が適応される例
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: const Text("Card Surface Color"),
+                  ),
+                ),
+                const SizedBox(height: 16),
+
+                /// outline が適応される例
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text("Outline で枠線を指定している例"),
+                ),
+
+                const SizedBox(height: 16),
+
+                // elevatedButtonの設定が適応される例
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("ElevatedButton"),
+                ),
+
+                // themeの切り替え
+                const SettingsSwitchTile(
+                  title: "theme切替え",
+                  icon: Icons.brightness_4,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+
+      // FloatingActionButtonの設定が適応される例
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // ボタンが押されたときにダイアログを表示する
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Dialog Title"),
+                content: const Text("This is a simple dialog."),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text("Close"),
+                    onPressed: () {
+                      Navigator.of(context).pop(); // ダイアログを閉じる
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_theme/app.dart';
 import 'package:flutter_theme/constant/shared_preferences.dart';
-import 'package:flutter_theme/theme_color_repository/theme_color_repository.dart';
-import 'package:flutter_theme/theme_data/theme_dark.dart';
-import 'package:flutter_theme/theme_data/theme_light.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -17,24 +14,7 @@ Future<void> main() async {
       overrides: [
         sharedPreferencesProvider.overrideWithValue(sharedPreferences),
       ],
-      child: const MainApp(), // MainAppを使用
+      child: const App(),
     ),
   );
-}
-
-class MainApp extends ConsumerWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final themeMode = ref.watch(themeColorRepositoryProvider);
-
-    return MaterialApp(
-      title: 'Theme Tips',
-      theme: themeLight(),
-      darkTheme: themeDark(),
-      themeMode: themeMode,
-      home: const MyApp(),
-    );
-  }
 }
